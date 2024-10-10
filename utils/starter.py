085d56f43d2a9b0b0034aa91fd1056560b5dde94
hhhscvx <hhhscvx@yandex.ru>
2024-10-10 12:32:25 +0500

    Starter: tasks

--- utils/starter.py
+++ utils/starter.py
@@ -1,6 +1,5 @@
 import asyncio
 from pprint import pprint
-from random import randint, uniform
 from typing import NoReturn
 
 from pyrogram import Client
@@ -15,13 +14,30 @@ async def start(tg_client: Client, proxy: str | None = None) -> NoReturn:
     session_name = tg_client.name + '.session'
 
     while True:
-        await pirateslot.login()
+        try:
+            await pirateslot.login()
+            me = await pirateslot.get_me()
+            logger.success(f"{session_name} | Signed in! Balance: {me.get('storedPirBalance')}")
 
-        me = await pirateslot.get_me()
-        pprint(me)
-        logger.info(me)
+            # Claim
+            
 
-        await asyncio.sleep(5)
-        break
+            # Tasks
+            tasks_list = await pirateslot.task_list()
+            for task in tasks_list:
+                if task['completed'] is False:
+                    complete = await pirateslot.complete_task(task_id=task['id'])
+                    if complete is not None:
+                        logger.success(f"{session_name} | Complete task «{task['name']}» | Earned +{task['reward']} PIR")
 
-        ...
+            # Game
+
+
+            await asyncio.sleep(5)
+            break
+
+            ...
+
+        except Exception as error:
+            logger.error(f"{session_name} | Unknown Error: {error}")
+            await asyncio.sleep(delay=3)
